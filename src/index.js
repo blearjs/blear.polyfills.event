c59effdb879d559e99be25832fa35941ba757e3a
cloudcome <cloudcome@163.com>
2016-05-17 17:13:16 +0800

    # blear.polyfills.event

---
+++ src/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+if (typeof CLASSICAL !== 'undefined' && CLASSICAL === true) {
+    !window.addEventListener && (function (WindowPrototype, DocumentPrototype, ElementPrototype, addEventListener, removeEventListener, dispatchEvent, registry) {
+        WindowPrototype[addEventListener] = DocumentPrototype[addEventListener] = ElementPrototype[addEventListener] = function (type, listener) {
+            var target = this;
+
+            registry.unshift([target, type, listener, function (event) {
+                event.currentTarget = target;
+                event.preventDefault = function () {
+                    event.returnValue = false
+                };
+                event.stopPropagation = function () {
+                    event.cancelBubble = true
+                };
+                event.target = event.srcElement || target;
+
+                listener.call(target, event);
+            }]);
+
+            this.attachEvent("on" + type, registry[0][3]);
+        };
+
+        WindowPrototype[removeEventListener] = DocumentPrototype[removeEventListener] = ElementPrototype[removeEventListener] = function (type, listener) {
+            for (var index = 0, register; register = registry[index]; ++index) {
+                if (register[0] == this && register[1] == type && register[2] == listener) {
+                    return this.detachEvent("on" + type, registry.splice(index, 1)[0][3]);
+                }
+            }
+        };
+
+        WindowPrototype[dispatchEvent] = DocumentPrototype[dispatchEvent] = ElementPrototype[dispatchEvent] = function (eventObject) {
+            return this.fireEvent("on" + eventObject.type, eventObject);
+        };
+
+        window.Event = function Event(type, eventInitDict) {
+            if (!type) {
+                throw new Error('Not enough arguments');
+            }
+
+            // Shortcut if browser supports createEvent
+            if ('createEvent' in document) {
+                var event = document.createEvent('Event');
+                var bubbles = eventInitDict && eventInitDict.bubbles !== undefined ? eventInitDict.bubbles : false;
+                var cancelable = eventInitDict && eventInitDict.cancelable !== undefined ? eventInitDict.cancelable : false;
+
+                event.initEvent(type, bubbles, cancelable);
+
+                return event;
+            }
+
+            var event = document.createEventObject();
+
+            event.type = type;
+            event.bubbles = eventInitDict && eventInitDict.bubbles !== undefined ? eventInitDict.bubbles : false;
+            event.cancelable = eventInitDict && eventInitDict.cancelable !== undefined ? eventInitDict.cancelable : false;
+
+            return event;
+        };
+    })(Window.prototype, HTMLDocument.prototype, Element.prototype, "addEventListener", "removeEventListener", "dispatchEvent", []);
+}
